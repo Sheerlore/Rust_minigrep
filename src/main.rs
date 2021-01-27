c10e1ce6140b69a8a373722bf82b58e0b04db4d0
uai <noema01@outlook.jp>
2021-01-27 11:36:17 +0900

    add poem.txt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
-    println!("minigrep project!");
 
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    // search {}
+    println!("Searching for {}", query);
+    // contents of file in {}
+    println!("In file {}", filename);
+
+    let mut f = File::open(filename).expect("file not found");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
     
+    println!("With text: \n{}", contents);
 }
